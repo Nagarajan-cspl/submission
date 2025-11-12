b57b8d3ae60249981097a5bb84f6afe0de9fcd44
Nagarajan-cspl <nagarajan.r@cspl.com>
2025-11-12 21:57:13 +0530

    Delete submission/cw directory

--- submission/cw/About.jsx
+++
@@ -1,5 +0,0 @@
-// About.jsx
-export default function About() {
-    return <h2>ℹ️ About Page</h2>;
-  }
-  
--- submission/cw/isActive.js
+++
@@ -1,6 +0,0 @@
-// src/isActive.js
-export default function isActive() {
-    const token = localStorage.getItem("token");
-    return !!token; // true if logged in, false otherwise
-  }
-  
