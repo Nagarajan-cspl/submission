de20b43228c79ecaa81ab6fef83da49d107dea57
Nagarajan-cspl <nagarajan.r@cspl.com>
2025-11-12 21:51:47 +0530

    Create FormSlice.js

---
+++ cw/React-proj2/FormSlice.js
@@ -0,0 +1,32 @@
+// src/formSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+const initialState={
+    data:{},
+    submitted:false,
+    token:{},
+    isLoggedIn: false
+}
+const FormSlice = createSlice({
+  name: 'loginForm',
+  initialState,reducers: {
+    fetchdata:(state)=>{
+        state.token={};
+    },
+    updateField:(state,action)=>{
+        const { field, value } = action.payload;
+        state.data[field]=value;
+    },
+
+    submitForm: (state) => {
+      state.submitted=true;
+    },
+    resetForm: (state) => {
+      state.data ={};
+      state.submitted = false;
+      state.token={};
+    },
+},
+});
+
+export const { updateField, submitForm, resetForm } = FormSlice.actions;
+export default FormSlice.reducer;
